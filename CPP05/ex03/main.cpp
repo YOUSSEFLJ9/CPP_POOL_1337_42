fe83e5bff435b3f5da69d7b45ad7df0a6deff67b
YOUSSEFLJ9 <youssefmomen@outlook.com>
2024-11-23 15:23:42 +0100

    all good

--- CPP05/ex03/main.cpp
+++ CPP05/ex03/main.cpp
@@ -6,27 +6,28 @@
 /*   By: ymomen <ymomen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 11:44:05 by ymomen            #+#    #+#             */
-/*   Updated: 2024/11/22 23:54:20 by ymomen           ###   ########.fr       */
+/*   Updated: 2024/11/23 15:23:36 by ymomen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
 #include "AForm.hpp"
 
-
 int main()
 {
     try
-    {Bureaucrat bureaucrat("zak", 1);
+    {
+		Bureaucrat Luis("luis", 5);
 		Intern intern;
-		AForm *form1;
-		AForm *form2;
-		form1 = intern.makeForm("Roboto", "Bender");
-		form2 = intern.makeForm("Shrubbery", "saim");
+		AForm *dossier1;
+		AForm *dossier2;
+		dossier1 = intern.makeForm("Presidential", "kingForm");
+		dossier2 = intern.makeForm("Shrubbery", "alber");
+		std::cout <<"_________________________________\n";
 		try
 		{
-			bureaucrat.signForm(*form1);
-			bureaucrat.signForm(*form2);
+			Luis.signForm(*dossier1);
+
 		}
 		catch(const std::exception& e)
 		{
@@ -34,15 +35,15 @@ int main()
 		}
 		try
 		{
-			bureaucrat.executeFrom(*form1);
-			form2->execute(bureaucrat);
+			Luis.executeFrom(*dossier1);
+			Luis.executeFrom(*dossier2);
 		}
 		catch(const std::exception& e)
 		{
 			std::cerr << e.what() << '\n';
 		}
-		delete form1;
-		delete form2;
+		delete dossier1;
+		delete dossier2;
     }
     catch(const std::exception& e)
     {
